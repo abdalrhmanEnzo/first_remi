8460e2809398dfdd2ca91eaa8059d7b268ed8535
dolz <mohamed.ramadan.adly@hotmail.com>
2015-07-08 15:45:33 +0300

    remove pagination from settings.py file.

--- first_remi/urls.py
+++ first_remi/urls.py
@@ -10,6 +10,5 @@ router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
